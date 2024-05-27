091c26a6e886cbeace05fd992727850a4c1cc402
abdulrahemfaqih <faqih3935@gmail.com>
2024-05-28 05:22:48 +0700

    user profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -12,14 +12,14 @@
     <div
         class="absolute top-28 lg:top-28 rounded-full md:top-48 mt-2 mx-auto w-32 h-32 left-1/2 right-1/2 translate-x-[-50%] z-40 overflow-hidden content-center">
         <img src="{{ asset('assets/images/alvan-nee-ZCHj_2lJP00-unsplash.jpg') }}" alt="profile" class="object-cover">
-        
+
     </div>
 
     <div class="z-40 absolute left-1/2 right-1/2 translate-x-[-50%] w-3/4 mx-auto top-[38%] font-bold text-center">
         <p class="text-lg">Alex Solihin </p>
         <p class="text-secondary text-sm opacity-40">Kamal, Jawa Timur</p>
     </div>
-    
+
     <div class="z-40 absolute left-1/2  justify-center  right-1/2 content-center translate-x-[-50%] w-3/4 mx-auto top-[50%] font-bold text-center"
         x-data="{ open: false }">
         <div class="mx-auto w-1/2 text-secondary flex flex-col" x-on:click="open = ! open">
@@ -29,10 +29,11 @@
         </div>
 
         {{-- form edit profile --}}
-        <form class="w-full max-w-lg mx-auto mt-6 p-6 " x-show="open" >
+        <form class="w-full max-w-lg mx-auto mt-6 p-6 " x-show="open">
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full  px-3 mb-6 md:mb-0 ">
-                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-first-name">
                         Username
                     </label>
                     <input
@@ -40,7 +41,8 @@
                         id="grid-first-name" type="text">
                 </div>
                 <div class="w-full  px-3">
-                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-last-name">
                         Nama
                     </label>
                     <input
@@ -50,7 +52,8 @@
             </div>
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full px-3">
-                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-password">
                         Email
                     </label>
                     <input
@@ -61,7 +64,8 @@
             </div>
             <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-full  px-3 mb-6 md:mb-0">
-                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-city">
                         Alamat
                     </label>
                     <input
@@ -70,8 +74,9 @@
                 </div>
             </div>
             <div class="flex flex-wrap -mx-3 mb-2">
-                <div class="w-full  px-3 mb-6 md:mb-0 mt-4" >
-                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                <div class="w-full  px-3 mb-6 md:mb-0 mt-4">
+                    <label class="block uppercase text-start tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-city">
                         Foto Profile
                     </label>
                     <input
@@ -79,7 +84,8 @@
                         id="grid-city" type="file">
                 </div>
             </div>
-            <input type="submit" name="submit" id="" value="Kirim" class="bg-gray-400 border-2 px-4 py-2 mt-4 md:px-10 rounded-md">
+            <input type="submit" name="submit" id="" value="Kirim"
+                class="bg-gray-400 border-2 px-4 py-2 mt-4 md:px-10 rounded-md">
         </form>
     </div>
 
